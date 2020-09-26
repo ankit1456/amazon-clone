9de63c8ff7f1700e06551575612beee6af346de3
Ankit Tripathi <ankittripathi1456@email.com>
2020-09-26 16:26:08 +0530

    fixed syntax error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../CSS/Header.css";
+import "../CSS/header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
