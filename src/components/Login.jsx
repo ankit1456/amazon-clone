5378f058b48f8a8ffd822b2032d627d37b7a5194
Ankit Tripathi <ankittripathi1456@email.com>
2020-09-26 16:29:36 +0530

    fixed authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
     <div className="login">
       <Link to="/">
         <img
-          className="login__logo"
+          className="login__Logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png"
           alt=""
         />
